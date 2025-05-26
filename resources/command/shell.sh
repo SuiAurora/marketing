765789d0d62a3f2fb5f22495fd3ef9af021abb79
lizhuohang <571732668@qq.com>
2025-05-26 23:44:31 +0800

    add

--- resources/command/shell.sh
+++ resources/command/shell.sh
@@ -1,4 +1,11 @@
 docker run -d \
+  --name nacos-2.3.1 \
+  -e MODE=standalone \
+  -p 8848:8848 \
+  -p 9848:9848 \
+  nacos/nacos-server:v2.3.1
+
+docker run -d \
 --name mysql-ksf \
 --restart unless-stopped \
 -p 3306:3306 \
